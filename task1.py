05c5a09d4b0eebacc3ae654057d0059be8b250c4
abhinavob <abhinavobulareddy@gmail.com>
2025-08-21 22:46:25 +0530

    Calculate and display the following results: - Traffic & Usage Analysis (API Requests, Endpoints, Status Codes) - Performance Metrics (Response times for different endpoints)

--- task1.py
+++ task1.py
@@ -1,7 +1,6 @@
-with open("../test.log", "r", encoding="utf-8") as f:
-    endpoints = {}
+with open("timetable.log", "r", encoding="utf-8") as f:
+    endpointsCount = {}
     endpointResponseTimes = {}
-    responseTimes = []
     codes = {}
     unique_ids = []
     for line in f:
@@ -12,18 +11,55 @@ with open("../test.log", "r", encoding="utf-8") as f:
             code = details[2]
             responseTime = details[3]
 
-            if endpoint not in endpoints:
-                endpoints[endpoint] = 0
+            if endpoint not in endpointsCount:
+                endpointsCount[endpoint] = 0
                 endpointResponseTimes[endpoint] = []
-            endpoints[endpoint] += 1
+            endpointsCount[endpoint] += 1
+
             if code not in codes:
                 codes[code] = 0
             codes[code] += 1
 
-            responseTimes.append(responseTime)
+            unit = responseTime[-2:]
+            responseTime = float(responseTime[:-2])
+            if unit == "ms":
+                responseTime *= 1000
+
             endpointResponseTimes[endpoint].append(responseTime)
         
         if "[202" in line:
             id = line[line.find("[202")+1:-2]
             if id not in unique_ids:
                 unique_ids.append(id)
+
+# Calculations
+
+totalRequests = sum([count for count in endpointsCount.values()])
+
+def formattedTime(time):
+    if time >= 1000:
+        time /= 1000
+        unit = "ms"
+    else:
+        unit = "µs"
+    return str(round(time, 2)) + " " + unit
+
+# Display Results
+
+print("Traffic & Usage Analysis\n\nTotal API Requests Logged: " + str(totalRequests))
+
+print("\nEndpoint Popularity:")
+for k, v in sorted(endpointsCount.items(), key=lambda x: x[1], reverse=True):
+    print("  - " + k + ": " + str(v) + " requests (" + str(round(v/totalRequests*100, 1)) + "%)")
+
+print("\nHTTP Status Codes:")
+for k, v in sorted(codes.items(), key=lambda x: x[1], reverse=True):
+    print("  - " + k + ": " + str(v) + " times")
+
+print("\nPerformance Metrics\n")
+for endpoint in endpointResponseTimes:
+    print("Endpoint: " + endpoint)
+    responseTimes = endpointResponseTimes[endpoint]
+    avgTime = sum(responseTimes) / len(responseTimes)
+    print("  - Average Response Time: " + formattedTime(avgTime))
+    print("  - Max Response Time: " + formattedTime(max(responseTimes)))
