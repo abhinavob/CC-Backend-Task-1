b7f1817206d7ffde6d32501e2e0626bea7ad15d7
abhinavob <abhinavobulareddy@gmail.com>
2025-08-22 19:35:45 +0530

    Add comments, rename variables, format display

--- task1.py
+++ task1.py
@@ -1,59 +1,65 @@
+# Open the timetable.log file to read and parse the data
 with open("timetable.log", "r", encoding="utf-8") as f:
-    endpointsCount = {}
-    endpointResponseTimes = {}
-    codes = {}
-    uniqueIDs = []
-    uniqueIDsCount = {}
-    strategiesCount = {"backtracking": 0, "sampling": 0}
-    timetablesGenerated = []
 
+    # Make dictionaries and lists to keep track of various data
+    endpoints_count = {}
+    endpoint_response_times = {}
+    codes = {}
+    unique_ids = []
+    unique_ids_count = {}
+    strategies_count = {"backtracking": 0, "sampling": 0}
+    timetables_generated = []
+
+    # Read the logs line by line, checking each line for any data we are interested in
     for line in f:
+
+        # Check for POST request and parse the endpoint, status code, and time of the response
         if "POST" in line:
             log = line[line.find("POST"):]
             details = log.split()
             endpoint = details[1]
             code = details[2]
-            responseTime = details[3]
+            response_time = details[3]
 
-            if endpoint not in endpointsCount:
-                endpointsCount[endpoint] = 0
-                endpointResponseTimes[endpoint] = []
-            endpointsCount[endpoint] += 1
+            if endpoint not in endpoints_count:
+                endpoints_count[endpoint] = 0
+                endpoint_response_times[endpoint] = []
+            endpoints_count[endpoint] += 1
 
             if code not in codes:
                 codes[code] = 0
             codes[code] += 1
 
-            unit = responseTime[-2:]
-            responseTime = float(responseTime[:-2])
+            unit = response_time[-2:]
+            response_time = float(response_time[:-2])
             if unit == "ms":
-                responseTime *= 1000
+                response_time *= 1000
 
-            endpointResponseTimes[endpoint].append(responseTime)
+            endpoint_response_times[endpoint].append(response_time)
         
+        # Check for unique IDs every time an ID appears in a line
         if "[202" in line:
             id = line[line.find("[202")+1:-2]
             batch = id[:4]
-            if batch not in uniqueIDsCount:
-                uniqueIDsCount[batch] = 0
-            if id not in uniqueIDs:
-                uniqueIDs.append(id)
-                uniqueIDsCount[batch] += 1
+            if batch not in unique_ids_count:
+                unique_ids_count[batch] = 0
+            if id not in unique_ids:
+                unique_ids.append(id)
+                unique_ids_count[batch] += 1
         
+        # Check for the strategy used for generating the timetables, and parse the number of timetables generated
         if "Backtracking" in line:
-            strategiesCount["backtracking"] += 1
+            strategies_count["backtracking"] += 1
         elif "Sampling" in line:
-            strategiesCount["sampling"] += 1
+            strategies_count["sampling"] += 1
         if "Generation" in line:
-            generatedNum = line[line.find("Found"):].split()[1]
-            timetablesGenerated.append(int(generatedNum))
+            generated_num = line[line.find("Found"):].split()[1]
+            timetables_generated.append(int(generated_num))
 
 
-# Calculations
+# Function for comparing and printing time with proper units
 
-totalRequests = sum([count for count in endpointsCount.values()])
-
-def formattedTime(time):
+def formatted_time(time):
     if time >= 1000:
         time /= 1000
         unit = "ms"
@@ -63,31 +69,33 @@ def formattedTime(time):
 
 # Display Results
 
-print("Traffic & Usage Analysis")
-print("\nTotal API Requests Logged: " + str(totalRequests))
+total_requests = sum([count for count in endpoints_count.values()])
+print("-"*30 + "\nTraffic & Usage Analysis\n" + "-"*30)
+print("Total API Requests Logged: " + str(total_requests))
 print("\nEndpoint Popularity:")
-for k, v in sorted(endpointsCount.items(), key=lambda x: x[1], reverse=True):
-    print("  - " + k + ": " + str(v) + " requests (" + str(round(v/totalRequests*100, 1)) + "%)")
+for k, v in sorted(endpoints_count.items(), key=lambda x: x[1], reverse=True):
+    print("  - " + k + ": " + str(v) + " requests (" + str(round(v/total_requests*100, 1)) + "%)")
 print("\nHTTP Status Codes:")
 for k, v in sorted(codes.items(), key=lambda x: x[1], reverse=True):
     print("  - " + k + ": " + str(v) + " times")
 
-print("\nPerformance Metrics\n")
-for endpoint in endpointResponseTimes:
+print("\n" + "-"*30 + "\nPerformance Metrics\n" + "-"*30)
+for endpoint in endpoint_response_times:
     print("Endpoint: " + endpoint)
-    responseTimes = endpointResponseTimes[endpoint]
-    avgTime = sum(responseTimes) / len(responseTimes)
-    print("  - Average Response Time: " + formattedTime(avgTime))
-    print("  - Max Response Time: " + formattedTime(max(responseTimes)))
+    response_times = endpoint_response_times[endpoint]
+    avg_time = sum(response_times) / len(response_times)
+    print("  - Average Response Time: " + formatted_time(avg_time))
+    print("  - Max Response Time: " + formatted_time(max(response_times)))
 
-print("\nApplication-Specific Insights\n")
+print("\n" + "-"*30 + "\nApplication-Specific Insights\n" + "-"*30)
 print("Timetables Generated:")
-print("  - Total: " + str(sum(timetablesGenerated)))
-print("  - Average: " + str(round(sum(timetablesGenerated) / len(timetablesGenerated), 2)))
+print("  - Total: " + str(sum(timetables_generated)))
+print("  - Average: " + str(round(sum(timetables_generated) / len(timetables_generated), 1)))
 print("\nTimetable Generation Strategy Usage")
-print("  - Heuristic Backtracking: " + str(strategiesCount["backtracking"]))
-print("  - Iterative Random Sampling: " + str(strategiesCount["sampling"]))
+print("  - Heuristic Backtracking: " + str(strategies_count["backtracking"]))
+print("  - Iterative Random Sampling: " + str(strategies_count["sampling"]))
 
-print("\nUnique ID Analysis\n\nTotal Unique IDs Found: " + str(sum(uniqueIDsCount.values())))
-for k, v in sorted(uniqueIDsCount.items(), key=lambda x: x[0]):
+print("\n" + "-"*30 + "\nUnique ID Analysis\n" + "-"*30)
+print("Total Unique IDs Found: " + str(sum(unique_ids_count.values())))
+for k, v in sorted(unique_ids_count.items(), key=lambda x: x[0]):
     print("Batch of " + k + ": " + str(v) + " unique IDs")
